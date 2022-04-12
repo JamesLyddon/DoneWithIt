46e95c1c43e2ab97e1ddad75d0439c17e57f4318
JamesLyddon <jameslyddon@gmail.com>
2022-04-12 16:20:53 +0100

    implemented centered bgImage and title

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ImageBackground,
+} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ImageBackground
+        style={{
+          width: wp("100%"),
+          height: hp("110%"),
+          resizeMode: "cover",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        source={{ uri: "https://picsum.photos/800/800" }}
+      >
+        <Text style={{ color: "#eeeee4", fontSize: 80 }}>Title</Text>
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
